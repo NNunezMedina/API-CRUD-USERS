73878876f24ad9869a3818ee51b97cc23e671b74
Nathaly <nathalynunezmedina@gmail.com>
2024-09-09 20:08:16 -0500

    Add .gitignore to exclude sensitive files

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,14 +1,18 @@
 // /src/config/database.ts
 
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+// Carga las variables de entorno desde el archivo .env
+dotenv.config();
 
 const pool = new Pool({
-  user: 'nathaly', // tu nombre de usuario en PostgreSQL
-  host: 'localhost', // el host donde está corriendo PostgreSQL
-  database: 'timetracker', // el nombre de tu base de datos
-  password: 'carjajeh12', // tu contraseña de PostgreSQL
-  port: 5432, // el puerto por defecto de PostgreSQL
-});
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT || '5432'),
+  });
 
 pool.on('connect', () => {
   console.log('Connected to the PostgreSQL database');
